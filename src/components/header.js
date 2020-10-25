f08c1d06f1a085faf589e269adb2dc00f95924e9
Maximilian Roll <maxroll54@gmail.com>
2020-10-25 16:16:30 +0100

    added yeahmap logo

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 const Header = () => {
     return(
-        <header>Header</header>
+        <header>
+            <div className="header-cont">
+                <div className="row space-between">
+                    <div className="col">
+                        <NavLink className="yeahmap-logo" to="/"><img src="yeahmap.png" alt=""/></NavLink>
+                    </div>
+                </div>
+            </div>
+        </header>
     )
 }
 
