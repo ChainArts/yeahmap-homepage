60abb3323b46b0420e23925f2cfd48d189ad445f
Maximilian Roll <maxroll54@gmail.com>
2020-11-24 22:49:38 +0100

    added width transitions

--- src/components/header.js
+++ src/components/header.js
@@ -1,6 +1,26 @@
+import { AnimatePresence, motion } from 'framer-motion';
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 const Header = () => {
+
+const navOverlay = {
+    hidden: {scale: 0, borderRadius: "500rem"},
+    open: {
+        scale: 1, borderRadius: "0rem",
+        transition: {
+            ease: [.14,.8,.4,1],
+            duration: 0.5
+        }
+    },
+    exit: {
+        scale: 0, borderRadius: "500rem",
+        transition: {
+            ease: [.14,.8,.4,1],
+            duration: 0.5
+        }
+    
+    }
+}
     const [isOpen, setIsOpen] = useState(false)
     return(
         <header>
@@ -10,12 +30,23 @@ const Header = () => {
                         <div className="yeahmap-logo" ><NavLink to="/" activeClassName=""><img src="yeahmap.png" alt="Home"/></NavLink></div>
                     </div>
                     <div className="col">
-                        <div className="hamburger-menu" onClick={e => setIsOpen(!isOpen)}>
+                        <div className="hamburger-menu desktop-hide" onClick={e => setIsOpen(!isOpen)}>
                             <div className={ isOpen ? "menu-toggle-icon menu-toggle-open" : "menu-toggle-icon menu-toggle-closed"}>
                                 <div className="bar"></div>
                                 <div className="bar"></div>
                                 <div className="bar"></div>
                             </div>
+                            <AnimatePresence exitBeforeEnter>
+                            {isOpen && (
+                            <motion.div className="menu-overlay" variants = {navOverlay} initial="hidden" animate = "open" exit="exit">
+                                <div className="menu-overlay-container">
+
+
+                                </div>
+                            </motion.div>
+                            )}
+
+                            </AnimatePresence>
                         </div>
                     </div>
                 </div>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -16,8 +16,8 @@ const NavMenu = {
 
 const Navbar = () => {
     return(
-        <motion.nav className="app-desktop-menu" variants = {NavMenu} initial="hidden" animate="open">
-            <ul className="menu mobile-hide">
+        <motion.nav className="app-desktop-menu mobile-hide" variants = {NavMenu} initial="hidden" animate="open">
+            <ul className="menu">
                 <li><NavLink to="/submit" activeClassName="current-page-item">SUBMISSION</NavLink></li>
                 <li><NavLink to="/who" activeClassName="current-page-item">WHO</NavLink></li>
                 <li><NavLink to="/socials" activeClassName="current-page-item">SOCIALS</NavLink></li>
